72f94103c7f485e579722f9bfa8db3545aa90279
neeraj15022001 <gneeraj32595@gmail.com>
2022-01-12 11:58:50 +0530

    done with folder structure

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const PORT = 8000;
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-//express router
+const expressLayouts = require("express-ejs-layouts")
+//set up the view engine
+app.use(express.static("./assets"))
+app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 app.use("/", require("./routes"))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+//express router
 app.listen(PORT, (err) => {
     if (err) {
         console.error("Error while running server");
