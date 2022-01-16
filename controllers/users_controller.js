f762a52c23b1c3ef18f8143d9ca9f696abe12c74
neeraj15022001 <gneeraj32595@gmail.com>
2022-01-16 23:43:14 +0530

    login done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,22 @@
 const User = require("../models/users_schema")
 module.exports.profile = (req, res) => {
-    return res.send("<h1>User Profile</h1>");
+    if (req.cookies.user_id) {
+        User.findById(req.cookies.user_id, (err, user) => {
+            if (err) {
+                console.log("Error finding user", err);
+                return res.redirect("/user/login")
+            } else {
+                if (user) {
+                    return res.render("profile", {title: "User Profile", user: user
+                    })
+                } else {
+                    return res.redirect("/users/login")
+                }
+            }
+        })
+    } else {
+        return res.redirect("/users/login")
+    }
 }
 
 module.exports.about = (req, res) => {
@@ -9,7 +25,6 @@ module.exports.about = (req, res) => {
 
 //render login page
 module.exports.login = (req, res) => {
-    console.log(req.cookies)
     return res.render('login', {layout: false, title: "Login"});
 }
 
@@ -50,5 +65,22 @@ module.exports.create = (req, res) => {
 }
 //create user session
 module.exports.createSession = (req, res) => {
-//    TODO
+    const userSigningIn = req.body;
+    User.findOne({email: userSigningIn.useremail}, function (err, user) {
+            if (err) {
+                console.log("Error finding user", err);
+                return;
+            }
+            if (user) {
+                if (user.password != userSigningIn.userpassword) {
+                    return res.redirect("back")
+                } else {
+                    res.cookie("user_id", user.id)
+                    return res.redirect("/users/profile")
+                }
+            } else {
+                return res.redirect('back');
+            }
+        }
+    )
 }
