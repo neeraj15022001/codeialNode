2d1d1c47b5cbe0cf3d514176cf2e102575f6154f
neeraj15022001 <gneeraj32595@gmail.com>
2022-01-21 02:04:43 +0530

    ui done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home_controller")
-const projectsController = require("../controllers/projects_controller")
 router.get("/", homeController.home)
-router.get("/projects", projectsController.projects)
+router.use("/posts", require("./posts"))
 router.use("/users", require("./users"))
 module.exports = router;
