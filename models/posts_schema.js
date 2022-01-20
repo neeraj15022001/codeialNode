bf4a159def17c4fd6ce000faf390f24047bbd88f
neeraj15022001 <gneeraj32595@gmail.com>
2022-01-20 13:33:42 +0530

    added posts schema

---
+++ models/posts_schema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const postsSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users"
+    }
+}, {
+    timestamps: true
+});
+
+const Posts = mongoose.model('Posts', postsSchema);
+module.exports = Posts;
